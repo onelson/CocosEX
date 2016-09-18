15d882bb646d5ec03428997f7f042261dccf6b44
Owen Nelson <onelson@gmail.com>
2016-09-18 00:29:16 -0700

    remove redundant calcs

--- Classes/systems/Spinner.cpp
+++ Classes/systems/Spinner.cpp
@@ -2,7 +2,6 @@
 
 void Spinner::update(ex::EntityManager &es, ex::EventManager &events, ex::TimeDelta dt) {
     es.each<Body>([this](ex::Entity entity, Body &body) {
-        int randNum = rand() % (50 - 0 + 1) + 0;
-        body.rotation += randNum;
+        body.rotation += rand() % 50;
     });
 }
